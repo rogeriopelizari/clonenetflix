e354b80638cd9c9fd013e451f926fb6a811ea27d
Rogerio Pelizari <rogpelizari@hotmail.com>
2020-09-05 02:47:18 -0300

    Projeto Clone Netflix - Estudo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default () => {  
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+    const [blackHeader, setBlackHeader] = useState(false);
+
+    useEffect(()=>{
+      const loadAll = async  () => {
+        //pegando a lista total
+        let list = await Tmdb.getHomeList();
+        setMovieList(list);
+
+        //Pegando o Featured
+        let originals = list.filter(i=>i.slug === 'originals');
+        let randonChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+        let chosen = originals[0].items.results[randonChosen];
+        let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+        setFeaturedData(chosenInfo);
+        //console.log(chosenInfo);
+      }
+        
+      loadAll();
+    }, []);          
+    
+    useEffect(()=>{
+        const scrollListener = () => {
+            if(window.scrollY > 10) {
+                setBlackHeader(true);
+            }else{
+                setBlackHeader(false);
+            }            
+        }
+
+        window.addEventListener('scroll', scrollListener);
+        
+        return() => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+    },    []);
+
+    return (
+        <div className="page"> 
+            
+            <Header black={blackHeader} />
+
+            {featuredData && 
+                <FeaturedMovie item={featuredData} />            
+            }
+            <section className="lists">
+                {movieList.map((item, key) =>(
+                    <MovieRow key={key} title={item.title} items={item.items} />
+                ))}
+            </section>
+
+            <footer>
+                Feito com  <span role="img" aria-label="coração">❤️</span> pela B7Web <br/>  
+                Direitos de imagem para Netflix <br/>
+                Dados pegos do site Themoviedb.org
+            </footer>   
+
+            {movieList.length <= 0 &&
+                <div className="loading">
+                    <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+                </div>     
+            }           
+
+        </div>
+    );
 }
-
-export default App;
